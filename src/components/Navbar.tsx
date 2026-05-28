27042d87e1f26ab1bf8586383aa312a58e7cc5e4
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-05-28 19:18:47 +0300

    feat: Added Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import {ChevronDown} from "lucide-react";
+import DropdownItem from "../ui/DropdownItem.tsx";
+import {useState} from "react";
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <nav className="flex items-center justify-center gap-16 fixed w-full bg-zinc-900/95 h-[55px]
+            border-b-2 border-zinc-800/90">
+                <span className="text-stef-red text-sm ibm font-medium">SM</span>
+                <div className="flex items-center gap-2">
+                    <div className="relative">
+                        <button
+                            className="flex items-center gap-1 text-stef-muted text-sm border border-zinc-700
+                            rounded-md px-3 py-1 hover:text-white hover:bg-stef-red hover:border-zinc-500 cursor-pointer"
+                            onClick={() => setIsOpen(!isOpen)}
+                        >
+                            Projects <ChevronDown size={14}/>
+                        </button>
+                        {isOpen && (
+                            <ul className='mt-2 absolute z-50 bg-zinc-900/95 text-center text-white w-[150px] rounded-md border border-zinc-700'>
+                                <DropdownItem href="#csharp" label="C#" addClasses="border-b border-zinc-700 rounded-t-md" />
+                                <DropdownItem href="#java" label="Java" addClasses="border-b border-zinc-700" />
+                                <DropdownItem href="#mean" label="MEAN" addClasses="rounded-b-md"/>
+                            </ul>
+                        )}
+                    </div>
+                    <a href="#about" className="flex items-center gap-1 text-stef-muted text-sm border
+                    border-zinc-700 rounded-md px-3 py-1 hover:text-white hover:bg-stef-red hover:border-zinc-500">
+                        About me
+                    </a>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
