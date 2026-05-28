34ff9088e65d0ffb7e39a4a226a6ccf95acba29c
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-05-28 21:42:57 +0300

    feat: Added Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar.tsx";
+import Hero from "./components/Hero.tsx";
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Hero/>
     </>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,25 @@
+import LineNumbers from "../ui/LineNumbers.tsx";
+import LinkButton from "../ui/LinkButton.tsx";
+
+const Hero = () => {
+    return (
+        <>
+            <section className="flex flex-col items-center justify-center relative min-h-screen
+            pt-[55px] gap-5 pb-20 dot-bg bg-black text-center text-white">
+                <LineNumbers highlight={1}/>
+                <span className='text-stef-red ibm'>JUNIOR FULL-STACK DEVELOPER</span>
+                <h1 className='text-8xl font-medium'>
+                    Stefanos <br/>
+                    <span className='text-stef-red'>Motsos</span>
+                </h1>
+                <span className={'text-stef-muted'}>Ready to build.</span>
+                <div className='flex items-center gap-1'>
+                    <LinkButton href={"#csharp"} label={"View Projects"}></LinkButton>
+                    <LinkButton href="#about" label={'About me'}></LinkButton>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Hero
